f6f70d0153d9a798082655fd5e21b9afe3f8f2b5
Anand-singh-123 <anandchauhan15798@gmail.com>
2024-06-08 10:21:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+require("dotenv")
+const express = require("express")
+
+const app = express()
+const PORT = process.env.PORT || 5050
+app.get("/test",async(req,res)=>{
+    return res.status(200).json({status:200,message:"server is up"})
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`server is started at port number ${PORT}`)
+})

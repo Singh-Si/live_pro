d8780ebe9c9876ea10cd5aab1b7cf80a3cf84b79
Anand-singh-123 <anandchauhan15798@gmail.com>
2024-06-08 18:54:42 +0530

    changes in app.js file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5050
 app.get("/test",async(req,res)=>{
     return res.status(200).json({status:200,message:"server is up"})
 })
-
+console.log("he anand")
 
 app.listen(PORT,()=>{
     console.log(`server is started at port number ${PORT}`)

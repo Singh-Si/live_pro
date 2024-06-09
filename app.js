43bba0018ddeae899a9d6dcf3f64423430a748c1
Anand-singh-123 <anandchauhan15798@gmail.com>
2024-06-09 10:55:37 +0530

    new route tset1

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 5050
 app.get("/test",async(req,res)=>{
     return res.status(200).json({status:200,message:"server is up"})
 })
+app.get("/test1",async(req,res)=>{
+    return res.status(200).json({status:200,message:"server is up at test1"})
+})
 console.log("he anand")
 
 app.listen(PORT,()=>{

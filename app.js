b6e3db51100aff699e735216ec2102578fa2a7cc
Anand-singh-123 <anandchauhan15798@gmail.com>
2024-06-09 11:26:40 +0530

    new route

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.get("/test",async(req,res)=>{
 app.get("/test1",async(req,res)=>{
     return res.status(200).json({status:200,message:"server is up at test1"})
 })
+app.get("/test3",async(req,res)=>{
+    return res.status(200).json({status:200,message:"server is up at test3"})
+})
 console.log("he anand")
 
 app.listen(PORT,()=>{
